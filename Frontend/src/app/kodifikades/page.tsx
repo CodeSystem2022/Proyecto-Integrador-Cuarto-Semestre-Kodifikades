a9246bedf3ba2c9f1955622d70f2c7a23da30d0c
Nicolas Ortiz <nicoortiz9870@gmail.com>
2023-11-13 11:33:55 -0300

    page kodifikades

---
+++ Frontend/src/app/kodifikades/page.tsx
@@ -0,0 +1,12 @@
+import KodifikadesCard from '@/app/components/kodifikades/page'
+
+
+export default function Kodifikades() {
+  return (
+    <>
+      <section className='mt-10 text-center'>
+        <h1 className='text-2xl font-bold mx-auto mb-9'>Sobre Nosotros</h1>
+        <KodifikadesCard/>
+      </section>
+    </>)
+}
